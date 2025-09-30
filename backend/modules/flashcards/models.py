3e50121ca298bd12bc880cc80f8ac80e92ba7de6
Marcos-Dantas22 <kisnaiver1223@gmail.com>
2025-09-29 22:10:29 -0300

    fix: configurado mappers para herança polimorfica dos tipos de flashcards #13

--- backend/modules/flashcards/models.py
+++ backend/modules/flashcards/models.py
@@ -16,6 +16,10 @@ class Flashcard(Base):
     created = Column("criado em", DateTime, default=datetime.now())
     last_update = Column("ultima atualização", DateTime, server_default=func.now(), onupdate=func.now())
     
+    __mapper_args__ = {
+        "polymorphic_identity": "flashcard",      
+        "polymorphic_on": flashcard_type,        
+    }
 
 class FlashcardBasic(Flashcard):
     __tablename__ = "flashcard_basic"
@@ -23,11 +27,19 @@ class FlashcardBasic(Flashcard):
     front_side_text = Column("frente", String, nullable=False)
     back_side_text = Column("verso", String, nullable=False)
 
+    __mapper_args__ = {
+        "polymorphic_identity": FlashcardTypes.BASIC,   
+    }
+
+
 class FlashcardChoose(Flashcard):
     __tablename__ = "flashcard_choose"
     id = Column(Integer, ForeignKey("flashcards.id"), primary_key=True)
     base_text = Column("texto base", String, nullable=False)
     keywords = Column("palavras-chave", JSON, nullable=False)
+    __mapper_args__ = {
+        "polymorphic_identity": FlashcardTypes.CHOOSE,
+    }
 
     @validates("keywords")
     def validate_keywords(self, key, keywords):
@@ -44,9 +56,6 @@ class FlashcardMultipleChoice(Flashcard):
     answer2 = Column("resposta 2", String, nullable=False)
     answer3 = Column("resposta 3", String, nullable=False)
 
-    # @validates("keywords")
-    # def validate_keywords(self, key, keywords):
-    #     for word in keywords:
-    #         if word not in self.base_text:
-    #             raise ValueError(f"A palavra-chave '{word}' não está presente em base_text")
-    #     return keywords
+    __mapper_args__ = {
+        "polymorphic_identity": FlashcardTypes.MULTIPLE_CHOICE,
+    }
